8b3e25bbc03e283ef7d1809d850e52bc107bdd34
Ishant <90744224+Agent8800@users.noreply.github.com>
2024-06-17 13:56:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 from pyrogram import Client, filters
 
 # Replace with your bot token
-API_TOKEN = "7033499587:AAE9ZC3gAetGvISzqFlzY8etuRmTYwCjfP8"
+BOT_TOKEN = "7033499587:AAE9ZC3gAetGvISzqFlzY8etuRmTYwCjfP8"
+
+# Replace with your API ID
+API_ID = "7391573"
+
+# Replace with your API hash
+API_HASH = "1f20df54dfd91bcee05278d3b01da2c7"
 
 # Create a Pyrogram client
-app = Client("animesia_bot", api_id=API_TOKEN, api_hash="1f20df54dfd91bcee05278d3b01da2c7")
+app = Client("animesia_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Define a filter for the start command
 start_filter = filters.command("start")
@@ -27,4 +33,5 @@ async def start_message(client, message):
     # await message.reply_video(media, caption=text, parse_mode="markdown")
 
 # Run the bot
-app.run()
+if __name__ == "__main__":
+    app.run()
